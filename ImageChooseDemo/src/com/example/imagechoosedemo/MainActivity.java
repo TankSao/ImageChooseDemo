3f93ad1662e0ffa8257abbf0d245c2fe7d9515a0
T-TAO <38127423+TankSao@users.noreply.github.com>
2019-01-18 02:46:23 +0800

    2019-01-18

---
+++ ImageChooseDemo/src/com/example/imagechoosedemo/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.imagechoosedemo;
+
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+	ArrayList<String> listfile=new ArrayList<String>();
+	private TextView pick;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Bundle bundle= getIntent().getExtras();
+    	
+		if (bundle!=null) {
+			if (bundle.getStringArrayList("files")!=null) {
+				listfile= bundle.getStringArrayList("files");
+				Toast.makeText(this, "已经选择"+listfile.size()+"张照片", Toast.LENGTH_LONG).show();
+			}
+		}
+		pick = (TextView) findViewById(R.id.pick);
+		pick.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO 自动生成的方法存根
+				Intent intent = new Intent(MainActivity.this,ImageFloderActivity.class);
+				startActivity(intent);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
